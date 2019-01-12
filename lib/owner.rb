8eea1ebbbe76d967bb690c3fd82ec64619bde079
Aaron Berns <anberns@gmail.com>
2019-01-12 00:43:43 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,25 @@
 class Owner
-  # code goes here
+  
+  attr_accessor :name, :pets
+  attr_reader :species
+  
+  @@all = []
+  
+  def initialize
+    @species = "human"
+    @pets = {
+      :fishes => [],
+      :dogs => [],
+      :cats => []
+    }
+  end 
+  
+  def self.count
+    @@all.size 
+  end 
+  
+  def say_species
+    puts "I am a #{@species}."
+  end
+  
 end
